2eda750bf12eddc14fb0de15e5bcfb2a02030605
kjh0718 <ranbow3062961@gmail.com>
2026-04-27 19:31:13 +0900

    6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
