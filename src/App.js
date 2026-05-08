c060c33dd47d25965d0b3e3983187295bb4d07d5
kjh0718 <ranbow3062961@gmail.com>
2026-05-08 16:43:23 +0900

    react 간단 영화 웹

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
